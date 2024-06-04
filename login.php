896c315f14d4375220c380f8eeef10898aa06a3b
Ilanewos <ilanewos04@gmail.com>
2024-06-04 18:32:00 +0800

    Langkah 2 : Membuat login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DictionaryNet- Log In</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="box-login">
+    <p>Log-In</p>
+    <div class="login">
+        <form action="" method="post">
+            <label for="uname"><b>Username/Email</b></label><br>
+            <input type="text" placeholder="Username/Email" name="uname" required><br>
+            <label for="psw"><b>Password</b></label><br>
+            <input type="password" placeholder="Password" name="psw" required><br>
+            <button type="submit" class="btn">Login</button>
+        </form>
+    </div>
+        <div class="acc-sign"><a href="signup.php">Don't have an account? Sign up now</a></div>
+    </div>
+</body>
+</html>
